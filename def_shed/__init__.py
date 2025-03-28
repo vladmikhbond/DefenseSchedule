4824848471ce862de9b6a7070ea87ce441a705a6
vladmikhbond <vladmikhbond@gmail.com>
2025-03-28 21:15:25 +0200

    .

--- def_shed/__init__.py
+++ def_shed/__init__.py
@@ -3,10 +3,11 @@ from flask import Flask
 def create_app():
     """Фабрична функція для створення Flask-додатка"""
     app = Flask(__name__)
-     # Налаштування конфігурації (можна імпортувати з файлу)
+    
+    # Налаштування конфігурації (можна імпортувати з файлу)
     # app.config.from_object("config.Config")
 
-    # Імпорт маршрутів та реєстрація Blueprint'ів
+    # Імпорт та реєстрація Blueprint'ів
     from .routes import bp
     app.register_blueprint(bp)
 
